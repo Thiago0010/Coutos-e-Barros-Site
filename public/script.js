bb6a6ad4bf1f9969c6fa378c9684ec955ee5f458
Thiago.GT.NEV <caselasthiago@gmail.com>
2025-11-10 18:43:31 -0300

    pagina principal concluida, começando a 2 parte.

---
+++ public/script.js
@@ -0,0 +1,9 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+            event.preventDefault();
+            const name = document.getElementById('name').value;
+            const email = document.getElementById('email').value;
+            const message = document.getElementById('message').value;
+            const whatsappNumber = '55???????????'; // Substitua pelo número real de WhatsApp
+            const formattedMessage = encodeURIComponent(`Nome: ${name}\nEmail: ${email}\nMensagem: ${message}`);
+            window.location.href = `https://wa.me/${whatsappNumber}?text=${formattedMessage}`;
+        });
